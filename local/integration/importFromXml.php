add65790b86c9ebbec64db48e746fd5e528b2417
Alexey <alexey174@gmail.com>
2018-06-13 18:27:01 +0300

    Lesson 3 Получили список городов Скрипт по созданию товара с торговым предложением

--- local/integration/importFromXml.php
+++ local/integration/importFromXml.php
@@ -4,41 +4,51 @@ pre('start');
 #Что нужно сделать: Загрузить XML в PHP (рекомендую simplexml_load_file). Далее вывести на экран каждый элемент со свойствами
 #Цель: Аккуратно окунуть Вас в работу с самим PHP и посмотреть у кого возникнут сложности с чистым PHP. Далее мы имея данные в массивах/обьектах научимся загружать это непосредственно в Bitrix.
 #-------------КОНЕЦ КОДА
-
+$el = new CIBlockElement;
+$els = new CCatalogProduct;
 if (file_exists('data/data.xml')) {
     $xml = simplexml_load_file('data/data.xml');
     foreach ($xml->product as $product) {
-        foreach ($product as $item) {
-            echo $item;
-            if (is_object($item)) {
-                foreach ($item as $it) {
-                    echo $it;
-                    if (is_object($it)) {
-                        foreach ($it as $i) {
-                            echo $i;
-                            if (is_object($i)) {
-                                foreach ($i as $offer) {
-                                    echo $offer;
-                                    echo "<br>";
-                                }
-                            }
-                            echo "<br>";
-                        }
-                    }
-                    echo "<br>";
-                }
+        $id = $product->OLDID;
+        $name = $product->NAME;
+        $code = $product->CODE;
+        $description = $product->DESCRIPTION;
+        $images = array();
+        foreach ($product->IMAGES as $image=>$img){
+            for ($i = 0; $i < count($img); $i++){
+                array_push($images, $img->OPTION[$i]);
             }
-            //var_dump($item);
-
-            foreach ($item->OFFERS as $offer) {
-                echo $offer;
-                echo "<br>";
-            }
-
-            echo "<br>";
         }
-        echo "____________________________________________________";
-        echo "<br>";
+        $arFields = array(
+            "NAME" => $name,
+            "CODE" => $code,
+            "DETAIL_TEXT" => $description,
+            "IBLOCK_ID" => 4,
+        );
+        $item = $el->Add($arFields);
+        var_dump($item);
+        if (!$item){
+            break;
+        }
+        foreach ($product->OFFERS as $offers=>$offer) {
+            for ($i = 0; $i < count($offer); $i++) {
+                $sizeField = $offer->OFFER[$i]->SIZE_FIELD;
+                $gameType = $offer->OFFER[$i]->GAME_TYPE;
+                $arFields = array(
+                    "NAME" => $name,
+                    "CODE" => $code,
+                    "DETAIL_TEXT" => $description,
+                    "IBLOCK_ID" => 5,
+                    "PROPERTY_VALUES" => array(
+                        "SIZE_FIELD" => $sizeField,
+                        "GAME_TYPE" => $gameType,
+                        "CML2_LINK" => $item,
+                    ),
+                );
+                $off = $el->Add($arFields);
+            }
+        }
+        break;
     }
 } else {
     exit('Не удалось открыть файл data.xml.');
